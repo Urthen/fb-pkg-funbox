90831ef83515ae6a67967c2b0670f566b5f8c493
Michael Pratt <urthen@gmail.com>
2016-02-17 15:50:48 -0600

    Use new child loading syntax

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ module.exports = {
     description : 'Collection of other awesome modules.',
 
     children : [
-        require('fb-opt-facts'),
-        require('fb-opt-google'),
-        require('fb-opt-quotes'),
-        require('fb-opt-stocks'),
-        require('fb-opt-weather')
+        'fb-opt-facts',
+        'fb-opt-google',
+        'fb-opt-quotes',
+        'fb-opt-stocks',
+        'fb-opt-weather'
     ]
 };
